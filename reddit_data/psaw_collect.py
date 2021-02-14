57d04312b526457d61b2617e864a775b3a33c3d0
Nick-git56 <nikkomunoz@gmail.com>
2021-02-14 03:12:08 -0800

    cleaned up scripts for collecting reddit and twitter data

--- reddit_data/psaw_collect.py
+++ reddit_data/psaw_collect.py
@@ -25,23 +25,21 @@ avg_Dir = os.path.join(main_WorkingDir,"Daily_avg")
 data_Dir = os.path.join(main_WorkingDir,"redditStock_data")
 sub_Dir = os.path.join(data_Dir,"submission_data")
 com_Dir = os.path.join(data_Dir,"comments_data")
-# os.mkdir(avg_Dir)
-# os.mkdir(data_Dir)
-# os.chdir(data_Dir)
-# os.mkdir(sub_Dir)
-# os.mkdir(com_Dir)
-# os.chdir(main_WorkingDir)
-# print(os.path.abspath(os.curdir))
+
+os.mkdir(avg_Dir)
+os.mkdir(data_Dir)
+os.chdir(data_Dir)
+os.mkdir(sub_Dir)
+os.mkdir(com_Dir)
+os.chdir(main_WorkingDir)
+print(os.path.abspath(os.curdir))
 # sys.exit()
 
 sentimentAnalyser = SentimentIntensityAnalyzer()
 now = dt.datetime.now()
-#prev = now - dt.timedelta(days=2*365+1)
 
-# stocks_acronym = ['PANW','AMT','ADI','TMUS','AAPL','ERIC','QCOM','SQ','OKTA','COUP','MELI',
-#                     'PYPL','BABA', 'SHOP','NVDA','CRM','AMZN','MSFT','GOOGL']
-
-stocks_acronym = ['PYPL','BABA', 'SHOP','NVDA','CRM','AMZN','MSFT','GOOGL']
+stocks_acronym = ['PANW','AMT','ADI','TMUS','AAPL','ERIC','QCOM','SQ','OKTA','COUP','MELI',
+                    'PYPL','BABA', 'SHOP','NVDA','CRM','AMZN','MSFT','GOOGL']
 
 stocks_name = ['Palo Alto Networks','American Tower','Analog Devices','T-Mobile','Apple','Ericsson','Qualcomm',
                 'Square','Okta','Coupa Software','MercadoLibre','Paypal','Alibaba','Shopify','NVIDIA',
@@ -50,11 +48,6 @@ stocks_name = ['Palo Alto Networks','American Tower','Analog Devices','T-Mobile'
 #investing,stocks,wallstreetbets
 api = PushshiftAPI()
 
-#start_epoch=int(dt.datetime(2019, 1, 2).timestamp())
-#start_epoch = int(dt.datetime(prev.year,prev.month,prev.day).timestamp())
-#end_epoch = int(dt.datetime(2020, 1,10).timestamp())
-#end_epoch = int(dt.datetime(now.year,now.month,now.day).timestamp())
-
 df_sentiment = pd.DataFrame()   #empty dataframe
 temp_df = pd.DataFrame()
 df1 = pd.DataFrame()    #empty dataframe
@@ -62,8 +55,6 @@ df2 = pd.DataFrame()    #empty dataframe
 empty_dataframe = False
 for stock in stocks_acronym:
     for cnt in range(2*365+1):
-        #This may be possibly two days of data because of the logic designed below
-        # Need to find how to do a single day specifically
         date_temp = now - dt.timedelta(days=(2*365+1)-cnt)
         date_temp2 = now - dt.timedelta(days=(2*365+1)-cnt-1)
         date_iter = int(dt.datetime(date_temp.year,date_temp.month,date_temp.day).timestamp())
@@ -76,7 +67,17 @@ for stock in stocks_acronym:
                                     filter=['id','title', 'score','selftext','num_comments','last_updated'],
                                     limit=5, sort="desc", sort_type="score"))
         
-        df_sub = pd.DataFrame(data_sub)
+        try:
+            df_sub = pd.DataFrame(data_sub)
+        except:
+            #Error: ValueError: 7 columns passed, passed data had 8 columns
+            print(f"Stock: {stock}, after: {date_temp}, before: {date_temp2}")
+            print(data_sub)
+            if input("enter 'c' to continue(anything else to exit program): ")=='c':
+                continue
+            else:
+                sys.exit()
+
         try:
             df_sub['created_utc'] = df_sub['created_utc'].apply(lambda x: dt.datetime.fromtimestamp(x))
             df_sub['created'] = df_sub['created'].apply(lambda x: dt.datetime.fromtimestamp(x))
@@ -129,38 +130,6 @@ for stock in stocks_acronym:
                 df_sub["negative"] = [sentimentAnalyser.polarity_scores(str(v))['neg'] for v in df_sub["title"]]
                 df_sub["positive"] = [sentimentAnalyser.polarity_scores(str(v))['pos'] for v in df_sub["title"]]
                 df_sub["neutral"] = [sentimentAnalyser.polarity_scores(str(v))['neu'] for v in df_sub["title"]]
-        # elif ('title' in df_sub.columns):
-        #     df_sub["combined_text"]= df_sub['title']
-        #     df_sub["compound"] = [sentimentAnalyser.polarity_scores(v)['compound'] for v in df_sub["combined_text"]]
-        #     df_sub["negative"] = [sentimentAnalyser.polarity_scores(v)['neg'] for v in df_sub["combined_text"]]
-        #     df_sub["positive"] = [sentimentAnalyser.polarity_scores(v)['pos'] for v in df_sub["combined_text"]]
-        #     df_sub["neutral"] = [sentimentAnalyser.polarity_scores(v)['neu'] for v in df_sub["combined_text"]]
-            
-        # for index,row in df_sub.iterrows():
-        #     if 'selftext' in df_sub.columns or 'title' in df_sub.columns:
-        #         try:
-        #             row["compound"] = sentimentAnalyser.polarity_scores(row['selftext'])['compound']
-        #             row["negative"] = sentimentAnalyser.polarity_scores(row['selftext'])['neg']
-        #             row["positive"] = sentimentAnalyser.polarity_scores(row['selftext'])['pos']
-        #             row["neutral"] = sentimentAnalyser.polarity_scores(row['selftext'])['neu']
-        #         except:
-        #             row["compound"] = sentimentAnalyser.polarity_scores(row['title'])['compound']
-        #             row["negative"] = sentimentAnalyser.polarity_scores(row['title'])['neg']
-        #             row["positive"] = sentimentAnalyser.polarity_scores(row['title'])['pos']
-        #             row["neutral"] = sentimentAnalyser.polarity_scores(row['title'])['neu']
-        #     # if (row['selftext'] == "") or (pd.isnull(row['selftext'])):
-        #     #     # this reddit post has no body text so use title
-        #     #     row["compound"] = sentimentAnalyser.polarity_scores(row['title'])['compound']
-        #     #     row["negative"] = sentimentAnalyser.polarity_scores(row['title'])['neg']
-        #     #     row["positive"] = sentimentAnalyser.polarity_scores(row['title'])['pos']
-        #     #     row["neutral"] = sentimentAnalyser.polarity_scores(row['title'])['neu']
-        #     # else:
-        #     #     row["compound"] = sentimentAnalyser.polarity_scores(row['selftext'])['compound']
-        #     #     row["negative"] = sentimentAnalyser.polarity_scores(row['selftext'])['neg']
-        #     #     row["positive"] = sentimentAnalyser.polarity_scores(row['selftext'])['pos']
-        #     #     row["neutral"] = sentimentAnalyser.polarity_scores(row['selftext'])['neu']
-        
-        #sys.exit()
         
         if not df_com.empty and ('body' in df_sub.columns):
             df_com["compound"] = [sentimentAnalyser.polarity_scores(v)['compound'] for v in df_com["body"]]
@@ -213,7 +182,6 @@ for stock in stocks_acronym:
         except:
             row['combined_text'] = row['title']
     if ('title' in df1.columns):
-        # df1["combined_text"]= df1['title'] + df1['selftext'] 
         try:
             df1["compound"] = [sentimentAnalyser.polarity_scores(str(v))['compound'] for v in df1["combined_text"]]
             df1["negative"] = [sentimentAnalyser.polarity_scores(str(v))['neg'] for v in df1["combined_text"]]
@@ -224,36 +192,7 @@ for stock in stocks_acronym:
             df1["negative"] = [sentimentAnalyser.polarity_scores(str(v))['neg'] for v in df1["title"]]
             df1["positive"] = [sentimentAnalyser.polarity_scores(str(v))['pos'] for v in df1["title"]]
             df1["neutral"] = [sentimentAnalyser.polarity_scores(str(v))['neu'] for v in df1["title"]]
-# elif ('title' in df1.columns):
-    #     df1["combined_text"]= df1['title']
-    #     df1["compound"] = [sentimentAnalyser.polarity_scores(v)['compound'] for v in df1["combined_text"]]
-    #     df1["negative"] = [sentimentAnalyser.polarity_scores(v)['neg'] for v in df1["combined_text"]]
-    #     df1["positive"] = [sentimentAnalyser.polarity_scores(v)['pos'] for v in df1["combined_text"]]
-    #     df1["neutral"] = [sentimentAnalyser.polarity_scores(v)['neu'] for v in df1["combined_text"]]
-    # # Have someone verify logic
-    # for index,row in df1.iterrows():
-    #     try:
-    #         row["compound"] = sentimentAnalyser.polarity_scores(row['selftext'])['compound']
-    #         row["negative"] = sentimentAnalyser.polarity_scores(row['selftext'])['neg']
-    #         row["positive"] = sentimentAnalyser.polarity_scores(row['selftext'])['pos']
-    #         row["neutral"] = sentimentAnalyser.polarity_scores(row['selftext'])['neu']
-    #     except:
-    #         row["compound"] = sentimentAnalyser.polarity_scores(row['title'])['compound']
-    #         row["negative"] = sentimentAnalyser.polarity_scores(row['title'])['neg']
-    #         row["positive"] = sentimentAnalyser.polarity_scores(row['title'])['pos']
-    #         row["neutral"] = sentimentAnalyser.polarity_scores(row['title'])['neu']
-    #     # if (row['selftext'] == "") or (pd.isnull(row['selftext'])):
-    #     #     # this reddit post has no body text so use title
-    #     #     row["compound"] = sentimentAnalyser.polarity_scores(row['title'])['compound']
-    #     #     row["negative"] = sentimentAnalyser.polarity_scores(row['title'])['neg']
-    #     #     row["positive"] = sentimentAnalyser.polarity_scores(row['title'])['pos']
-    #     #     row["neutral"] = sentimentAnalyser.polarity_scores(row['title'])['neu']
-    #     # else:
-    #     #     row["compound"] = sentimentAnalyser.polarity_scores(row['selftext'])['compound']
-    #     #     row["negative"] = sentimentAnalyser.polarity_scores(row['selftext'])['neg']
-    #     #     row["positive"] = sentimentAnalyser.polarity_scores(row['selftext'])['pos']
-    #     #     row["neutral"] = sentimentAnalyser.polarity_scores(row['selftext'])['neu']
-    # print(df1)
+    
     df1['weekly_average'] = df1['compound'].rolling(7, win_type='triang').sum()
     df1['monthly_average'] = df1['compound'].rolling(30, win_type='triang').sum()
 
@@ -279,11 +218,3 @@ for stock in stocks_acronym:
     df2 = pd.DataFrame()    #empty dataframe
     df_sub = pd.DataFrame()
     df_com = pd.DataFrame()
-
-
-
-
-# df['weekly_average'] = df['compound'].rolling(7, win_type='triang').sum()
-# df['monthly_average'] = df['compound'].rolling(30, win_type='triang').sum()
-
-# print(df)
--- twitter_scrapper/sns_collect2.py
+++ twitter_scrapper/sns_collect2.py
@@ -6,9 +6,7 @@ import datetime as dt
 now = dt.datetime.now()
 prev = now - dt.timedelta(days=2*365+1)
 
-# stocks_acronym = ['PANW','AMT','ADI','TMUS','AAPL','ERIC','QCOM','SQ','OKTA','COUP','MELI',
-#                     'PYPL','BABA', 'SHOP','NVDA','CRM','AMZN','MSFT','GOOGL']
-stocks_acronym = ['ADI','TMUS','AAPL','ERIC','QCOM','SQ','OKTA','COUP','MELI',
+stocks_acronym = ['PANW','AMT','ADI','TMUS','AAPL','ERIC','QCOM','SQ','OKTA','COUP','MELI',
                     'PYPL','BABA', 'SHOP','NVDA','CRM','AMZN','MSFT','GOOGL']
 
 stocks_name = ['Palo Alto Networks','American Tower','Analog Devices','T-Mobile','Apple','Ericsson','Qualcomm',
@@ -20,40 +18,6 @@ stocks_name = ['Palo Alto Networks','American Tower','Analog Devices','T-Mobile'
 
 # Creating list to append tweet data to
 tweets_list = []
-
-# # Using TwitterSearchScraper to scrape data and append tweets to list
-# for i,tweet in enumerate(sntwitter.TwitterSearchScraper('AMT since:2020-07-01 until:2020-09-05').get_items()):
-#     if i>maxTweets:
-#         break
-#     #tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.replyCount, tweet.retweetCount, tweet.likeCount, tweet.quoteCount])
-#     tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.likeCount])
-
-# # Creating a dataframe from the tweets list above
-# tweets_df = pd.DataFrame(tweets_list, columns=['Datetime', 'Tweet Id', 'Content', ])
-
-# # Display first 5 entries from dataframe
-# tweets_df.head()
-
-# # Export dataframe into a CSV
-# tweets_df.to_csv('text-query-tweets.csv', sep=',', index=False)
-
-# from nltk.sentiment.vader import SentimentIntensityAnalyzer
-# import nltk
-# nltk.download("vader_lexicon")
-# sentimentAnalyser = SentimentIntensityAnalyzer()
-# tweets_df["compound"] = [sentimentAnalyser.polarity_scores(v)['compound'] for v in tweets_df["Content"]]
-# tweets_df["negative"] = [sentimentAnalyser.polarity_scores(v)['neg'] for v in tweets_df["Content"]]
-# tweets_df["positive"] = [sentimentAnalyser.polarity_scores(v)['pos'] for v in tweets_df["Content"]]
-# tweets_df["neutral"] = [sentimentAnalyser.polarity_scores(v)['neu'] for v in tweets_df["Content"]]
-
-# tweets_df = tweets_df.sort_values(by="Datetime")
-
-# tweets_df['weekly_average'] = tweets_df['compound'].rolling(7, win_type='triang').sum()
-# tweets_df['monthly_average'] = tweets_df['compound'].rolling(30, win_type='triang').sum()
-
-# print(tweets_df)
-
-
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
